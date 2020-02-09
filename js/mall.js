c4f6066c7939b27911a89ad8178a83ba01dd8255
adel-addo <mostb18@gmail.com>
2020-02-09 23:23:47 +0000

    Central uni mall

---
+++ js/mall.js
@@ -0,0 +1,24 @@
+function myFunction(p1, p2) {
+    return p1 * p2;   // The function returns the product of p1 and p2
+  }
+
+title = 'cmall';
+searchTerm = '';
+isCollapsed = true;
+function token();
+{
+    return localStorage.getItem('token');
+}
+
+function collapse();
+{
+    this.isCollapsed = true;
+}
+function closeDropdown(dropdown);
+{
+    dropdown.close();
+}
+function logout();
+{ }
+function search();
+{ }
